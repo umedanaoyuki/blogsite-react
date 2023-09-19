a6398a679ba28a31e2ba824b4153d994144d4c24
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-19 22:50:13 +0900

    Headerだけ自分で作成（html分の完成）

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
 
-    const Header = () => {};
-
   return (
     <div className="App">
-      <Header />
+        <Header></Header>
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 
+//rcc
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <header>
+            <div className="logo">
+                <h3>ポートフォリオ</h3>
+            </div>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#">ホーム</a>
+                </li>
+                <li>
+                    <a href="#">ブログ</a>
+                </li>
+                <li>
+                    <a href="#">SNS</a>
+                </li>
+            </ul>
+        </nav>
+        </header>
     );
 };
 
