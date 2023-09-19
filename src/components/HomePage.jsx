d2ea2a049b79a5f3a032e1136900dcc8e60de293
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-09-19 23:25:51 +0900

    bootstrap ボタンの追加

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
+import {Button} from "react-bootstrap";
 
 class HomePage extends Component {
     render() {
         return (
             <div>
-                HomePage
+                <Button>ボタン</Button>
             </div>
         );
     }
